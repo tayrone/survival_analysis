a4b0642ab645c853d93f214de4707d01b66d8ba4
Tayrone Monteiro <tayronemonteiro@MacBook-Pro-de-Tayrone.local>
2021-03-09 17:10:25 -0300

    stat independency

--- survival_analysis.R
+++ survival_analysis.R
@@ -18,7 +18,7 @@ interest_regs <- c("BHLHE41", "CAMTA1", "ZNF365", "KCNIP3", "RFX4", "SOX2",
 
 enrich_scores <- tnsGet(rtns, "regulonActivity")
 
-status <- as.data.frame(enrich_scores$status)
+#status <- as.data.frame(enrich_scores$status)
 enrich_scores <- as.data.frame(enrich_scores$differential)
 
 survival_data <- tnsGet(rtns, "survivalData")
@@ -28,14 +28,14 @@ survival_data <- tnsGet(rtns, "survivalData")
 
 rownames(enrich_scores) <- str_remove(rownames(enrich_scores), ".CEL")
 rownames(survival_data) <- str_remove(rownames(survival_data), ".CEL")
-rownames(status) <- str_remove(rownames(status), ".CEL")
+#rownames(status) <- str_remove(rownames(status), ".CEL")
 
 
 #---- Some data wrangling ----
 
 score_stats <- enrich_scores %>% 
   select(all_of(interest_regs)) %>% 
-  summarise_all(list("mean"= mean, "median"= median, "iqr" = IQR))  %>% 
+  summarise_all(list("mean" = mean, "median" = median, "iqr" = IQR))  %>% 
   gather("method", "value") %>% 
   separate(method, c("regulon", "method"), sep = "_") %>% 
   spread(regulon, value)
@@ -43,33 +43,76 @@ score_stats <- enrich_scores %>%
 score_stats <- map(transpose(score_stats, .names = score_stats$method), 
                    as.vector)
 
-enrich_scores %>% 
-  select(CAMTA1) %>% 
-  mutate(iqr_value = score_stats[["median"]][["CAMTA1"]],
-         status = case_when(CAMTA1 > iqr_value ~ 1,
-                            near(CAMTA1, iqr_value) ~ 0,
-                            CAMTA1 < iqr_value ~ -1))
 
-calculate_status <- function(regulon_scores, stat){
-  print(names(regulon_scores))
-  
+calculate_status <- function(regulon, stat){
+  enrich_scores %>% 
+    select(regulon) %>% 
+    rownames_to_column(var = "sample") %>% 
+    mutate(stat_value = score_stats[[stat]][[regulon]],
+           status = case_when(get(regulon) >= stat_value ~ 1,
+                              near(get(regulon), stat_value) ~ 0,
+                              get(regulon) < stat_value ~ -1))
 }
 
-map(enrich_scores, calculate_status, stat = stats_median)
+interest_regs <- set_names(interest_regs, interest_regs)
+
+median_status <- map(interest_regs, calculate_status, stat = "median")
+
+mean_status <- map(interest_regs, calculate_status, stat = "mean")
+
+iqr_status <- map(interest_regs, calculate_status, stat = "iqr")
 
 
+#----
 
-# interest_status <- status %>% 
-#   select(all_of(interest_regs)) %>% 
-#   rownames_to_column(var = "sample")
-#   
-# status_and_survival <- survival_data %>% 
-#   rownames_to_column(var = "sample") %>% 
-#   select(sample, time, event) %>% 
-#   inner_join(interest_status)
+
+add_survival <- function(status){
+  
+status_and_survival <- 
+  survival_data %>%
+    rownames_to_column(var = "sample") %>%
+    select(sample, time, event) %>%
+    inner_join(status)
+
+}
+
+
+median_status <- map(median_status, add_survival)
+mean_status <- map(mean_status, add_survival)
+iqr_status <- map(iqr_status, add_survival)
+
+# complete_data <- list("median" = median_status, "mean" = mean_status, 
+#                       "iqr" = iqr_status)
 
 
 #---- Generates the KM estimation and subsequent plots ----
+
+
+
+km <- with(iqr_status[["CAMTA1"]], Surv(time = time, event = event))
+
+km_fit <- survfit(Surv(time = time, event = event) ~ get("CAMTA1"), 
+                  data = iqr_status[["CAMTA1"]])
+
+surv_pvalue(km_fit)$pval.txt
+surv_median(km_fit)
+print(km_fit)
+
+summary(km_fit, times = c(1,30,60,90*(1:10)))
+
+
+plot <- autoplot(km_fit, surv.linetype = "solid", conf.int = FALSE,
+                 censor.size = 3, surv.size = 0.5, main = "Kaplan Meier Estimate", 
+                 xlab = "Time (Years)", ylab = "Survival Rate") 
+
+plot + labs(color = "Regulon Activity Status") +
+  scale_color_discrete(labels = c("Up", "Neutral", "Down")) +
+  labs(tag = paste0("Log-rank p-value: ", round(surv_pvalue(km_fit)$pval, 3))) +
+  theme(plot.tag = element_text(size = 10, face = "plain"), 
+        plot.tag.position = c(.803, .985))
+
+
+
 
 for(regulon in interest_regs){
 
